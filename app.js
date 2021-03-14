d9505e98a291c2304511ea1b190a60fd4db078c2
souperwit <sirawit.mn@gmail.com>
2021-03-15 00:44:25 +0700

    all functions except authen

---
+++ app.js
@@ -0,0 +1,209 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+
+const myPw = "hNnxmWroEnFGYltd"
+
+app.set('view engine', 'ejs')
+
+
+app.use(methodOverride('_method'))
+app.use(bodyParser.json())
+// app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+
+//////////////// Mongoose setup
+
+// const url = 'mongodb+srv://manooi:hNnxmWroEnFGYltd@cluster0.0solc.mongodb.net/sitron_test?retryWrites=true&w=majority'
+// const db = mongoose.connection
+// const movie_records = db.collection('movies_records')
+
+
+// local
+const url = 'mongodb://localhost:27017/movieDB'
+mongoose.connect(url, {
+  useNewUrlParser: true
+});
+
+const movieSchema = {
+  title: String,
+  year: Number,
+  rating: String
+};
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+
+
+
+///////////////////// Request Targetting All  Movies
+// https://mongoosejs.com/docs/models.html
+
+app.route('/')
+.get(function(req, res) {
+  res.redirect('/movies')
+});
+
+
+
+app.route('/movies')
+.get(function(req, res) {
+  Movie.find(function(err, foundMovies) {
+    // console.log(foundMovies);
+    if (!err) {
+      res.render('sitron.ejs', {data:foundMovies})
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res) {
+  // console.log(req.body.title);
+  // console.log(req.body.content);
+  const newMovie = new Movie({
+    title: req.body.title,
+    year: req.body.year,
+    rating: req.body.rating,
+  });
+  newMovie.save(function(err) {
+    if (!err) {
+      // res.send('Successfully added a new movies.');
+      res.redirect('/movies')
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res) {
+  Movie.deleteMany(function(err) {
+    if (!err) {
+      // res.send('Successfully deleted all movies.');
+      res.redirect('/movies')
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+///////////////////// Request Targetting Specific Movies
+
+
+app.route('/movies/:moviesTitle')
+
+  .get(function(req, res) {
+
+    Movie.findOne({
+      title: req.params.moviesTitle
+    }, function(err, foundMovie) {
+      if (!err) {
+        res.send(foundMovie);
+      } else {
+        res.send("No movie matching that title was found.");
+      }
+    });
+  })
+
+  .put(function(req, res) {
+
+    Movie.update(
+    // Condition
+    {title:req.params.moviesTitle},
+    // Update
+    {title: req.body.title, year: req.body.year, rating: req.body.rating},
+    // Overwrite
+    {overwrite:true},
+    // Callback
+    function(err) {
+      if(!err) {
+        // res.send('Successfully updated movie.')
+        res.redirect('/movies')
+      } else {
+        res.send(err)
+      }
+    }
+  )}
+)
+
+  .patch(function(req, res) {
+
+    Movie.update(
+      // Condition
+      {title:req.params.moviesTitle},
+      // Set
+      {$set: req.body},
+      // Callback
+      function(err) {
+        if(!err) {
+          // res.send('Successfully updated movie.')
+          res.redirect('/movies')
+        } else {
+          res.send(err)
+        }
+      }
+    );
+  })
+
+  .delete(function(req, res) {
+    Movie.deleteOne(
+      // Condition
+      {title:req.params.moviesTitle},
+      // Callback
+      function(err) {
+        if(!err) {
+          // res.send('Successfully deleted movie.')
+          res.redirect('/movies')
+        } else {
+          res.send(err)
+        }
+      }
+  )
+});
+
+
+// Exposed API
+
+app.route('/api/movies')
+.get(function(req, res) {
+  Movie.find(function(err, foundMovies) {
+    // console.log(foundMovies);
+    if (!err) {
+      res.send(foundMovies)
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.route('/api/movies/:moviesTitle')
+  .get(function(req, res) {
+    Movie.findOne({
+      title: req.params.moviesTitle
+    }, function(err, foundMovie) {
+      if (!err) {
+        res.send(foundMovie);
+      } else {
+        res.send("No movie matching that title was found.");
+      }
+    });
+  });
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log('listening on port 3000')
+})
+
+
+
